8c02494a108a0ff80af9681f1b9b8a1dfb9fef0d
Vladimir Korotkov <vkorotkov@visionlabs.ru>
2026-01-31 23:35:48 +0300

    fix r20

--- app/devices/r20/r20.py
+++ app/devices/r20/r20.py
@@ -1,13 +1,20 @@
 import base64
-import json
 import time
 from datetime import datetime
 from pathlib import Path
 
 import requests
-
+from pydantic import BaseModel, Extra
 from app.devices.abc import DeviceSender
 from app.devices.r20.data import R20FACE_JSON
+
+
+class R20Event(BaseModel, extra="allow"):
+    base64: str
+    ip: str
+    tempUnit: str = None
+    temperature: str = None
+    time: str
 
 
 class R20Sender(DeviceSender):
@@ -19,15 +26,15 @@ class R20Sender(DeviceSender):
         for i, face in enumerate(faces):
             face_path = self._IMAGES_PATH.joinpath(face)
             try:
-                payload = self.get_request_body(face_path)
-                status = self.make_request(payload)
+                payload = self.get_json_body(face_path)
+                status = self.make_request(payload.model_dump())
                 result[face] = status
 
                 if progress_callback:
                     progress = ((i + 1) / len(faces)) * 100
                     progress_callback(face, status, progress)
 
-                time.sleep(3.1)
+                time.sleep(4)
 
             except Exception as e:
                 self.logger.error(f"Error processing {face}: {str(e)}")
@@ -41,19 +48,14 @@ class R20Sender(DeviceSender):
         return result
 
     def make_request(self, body: dict | bytes) -> int:
-        response = requests.request(method='POST', url=self._URL, data=body)
+        response = requests.request(method='POST', url=self._URL, json=body)
         if response.status_code != 200:
             self.logger.warning(f"Bad status {response.status_code}: Error {response.text}")
         return response.status_code
 
-    def get_request_body(self, photo_content: Path):
-        image_content = base64.b64encode(photo_content.read_bytes()).decode("utf8")
-        body = self._R20_BODY_TEMPLATE
-        body["base64"] = image_content
-        body["time"] = self.get_timestamp()
-        body_json = json.dumps(body)
-        payload = b'' + body_json.encode() + b'\n'
-        return payload
+    def get_json_body(self, photo: Path):
+        image_content = base64.b64encode(photo.read_bytes()).decode("utf-8")
+        return R20Event(base64=image_content, ip=self.host, tempUnit="1", temperature="36.6", time=self.get_timestamp())
 
     @staticmethod
     def get_timestamp():
