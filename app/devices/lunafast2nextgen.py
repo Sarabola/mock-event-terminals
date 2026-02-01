59ac5890a2582ba4d880a5cfda7c62495db78991
Vladimir Korotkov <vkorotkov@visionlabs.ru>
2026-02-01 21:16:45 +0300

    fix lunafast2 body

--- app/devices/lunafast2nextgen.py
+++ app/devices/lunafast2nextgen.py
@@ -3,30 +3,27 @@ import time
 from pathlib import Path
 
 import requests
+from pydantic import BaseModel
 
 from app.devices.abc import DeviceSender
 
 
+class LunaFast2Body(BaseModel):
+    bestshot_b64: str
+    isSigned: bool = False
+    device_id: str
+
+
 class LunaFast2NextGenSender(DeviceSender):
     _DEVICE_NAME = "lunafast2nextgen"
-
-    def get_request_body(self, _face: Path):
-        face_bytes = _face.read_bytes()
-        base_64 = base64.b64encode(face_bytes)
-        return {"bestshot_b64": base_64.decode("utf-8"), "isSigned": False, "device_id": self.device_id}
-
-    def make_request(self, face_body) -> int:
-        response = requests.request(method='POST', url=self._url, json=face_body)
-        if response.status_code != 200:
-            self.logger.warning(f"Bad status {response.status_code}: Error {response.text}")
-        return response.status_code
 
     def make_selected_photos_request(self, faces: list[str], progress_callback=None) -> dict[str, int]:
         result = {}
         for i, face in enumerate(faces):
             face_path = self._IMAGES_PATH.joinpath(face)
             try:
-                status = self.make_request(face_path)
+                json_data = self.get_request_body(face_path)
+                status = self.make_request(json_data)
                 result[face] = status
 
                 if progress_callback:
@@ -45,3 +42,14 @@ class LunaFast2NextGenSender(DeviceSender):
 
         self.logger.info("Sending successfully end.")
         return result
+
+    def get_request_body(self, _face: Path) -> LunaFast2Body:
+        face_bytes = _face.read_bytes()
+        base_64 = base64.b64encode(face_bytes).decode("utf-8")
+        return LunaFast2Body(bestshot_b64=base_64, isSigned=False, device_id=self.device_id)
+
+    def make_request(self, face_body) -> int:
+        response = requests.request(method='POST', url=self._url, json=face_body.model_dump())
+        if response.status_code != 200:
+            self.logger.warning(f"Bad status {response.status_code}: Error {response.text}")
+        return response.status_code
